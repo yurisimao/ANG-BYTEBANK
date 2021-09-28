c3ca9940011a8ce427ae3a7d435def3da756ecdc
Yuri Simão <yuri_simao@hotmail.com>
2021-09-28 20:24:59 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NewTransferComponent } from './new-transfer/new-transfer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTransferComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/new-transfer/new-transfer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-new-transfer',
+    templateUrl: './new-transfer.component.html',
+    styleUrls: ['./new-transfer.component.scss']
+})
+export class NewTransferComponent {
+
+}
